f33612a448065f9b69f69b477a475339bcfda202
mituramac <mitura.mac@gmail.com>
2025-07-16 22:43:33 +0200

    dealer outline

---
+++ common/dealer.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from deck import Deck
+
+class Dealer():
+    def __init__(self, deck: Deck):
+        self.deck = deck
+
+    def deal_card(self, destination: list[Card]):
+        pass
+
+
+if __name__ == "__main__":
+    pass
